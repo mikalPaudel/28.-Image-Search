12079a6a1a9aa03fe54fdb560f5f213c415b1392
Mikal Paudel <mikalpaudel58@gmail.com>
2025-10-18 23:53:17 +0545

    Implement image search functionality with API integration

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const searchResult = document.getElementById('search-result');
 const showMoreBtn = document.getElementById('show-more-btn');
 
 const accessKey = 'X2XuWE79RDm8LVXwL6lQXmgO99mBoaFPHZyefkynnFU';
+
+let keyword = '';
+let page = 1;
+
+async function searchImages() {
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        console.log(data);
+    }
+
+    searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImages();
+});
+
