8cb1532c314e64b8344a2850b2e63140c212c4ef
Mikal Paudel <mikalpaudel58@gmail.com>
2025-10-18 23:57:40 +0545

    Enhance image search results display by adding alt text and removing console log

--- script.js
+++ script.js
@@ -14,7 +14,17 @@ async function searchImages() {
         const response = await fetch(url);
         const data = await response.json();
 
-        console.log(data);
+        const results = data.results;
+
+        results.map((result) => {
+            const image = document.createElement('img');
+            image.src = result.urls.small;
+            const imageLink = document.createElement('a');
+            imageLink.href = result.links.html;
+            imageLink.target = '_blank';
+            imageLink.appendChild(image);
+            searchResult.appendChild(imageLink);
+        });
     }
 
     searchForm.addEventListener('submit', (e) => {
@@ -22,4 +32,6 @@ async function searchImages() {
     page = 1;
     searchImages();
 });
+
+
 
