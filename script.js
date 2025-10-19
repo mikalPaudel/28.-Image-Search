ace89861cdeafa371727c2bb53b21d39fa08a173
Mikal Paudel <mikalpaudel58@gmail.com>
2025-10-19 23:38:41 +0545

    Fix ID reference for search results in HTML and JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const searchForm = document.getElementById('search-form');
 const searchBox = document.getElementById('search-box');
-const searchResult = document.getElementById('search-result');
+const searchResult = document.getElementById('search-results');
 const showMoreBtn = document.getElementById('show-more-btn');
 
 const accessKey = 'X2XuWE79RDm8LVXwL6lQXmgO99mBoaFPHZyefkynnFU';
